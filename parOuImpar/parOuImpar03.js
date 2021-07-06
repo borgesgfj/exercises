6b3ee3c7e95f146e49d37d42890836ba1792c2ea
Gilberto Borges <borgesgfj@gmail.com>
2021-07-06 09:30:45 -0300

    Melhorando funções do exercício

--- parOuImpar/parOuImpar03.js
+++ parOuImpar/parOuImpar03.js
@@ -1,36 +1,52 @@
-const primeiro = [randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10)]
+const primeiro = [
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+];
 
-const segundo = [randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10), randomNumber(0, 10)]
+const segundo = [
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+  randomNumber(0, 10),
+];
 
 function randomNumber(min, max) {
-  return Math.floor(Math.random()*(max + 1 - min) + min)
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-let vitoriasPrimeiro = 0
-let vitoriasSegundo = 0
+let vitoriasPrimeiro = 0;
+let vitoriasSegundo = 0;
 
 function resultados(n1, n2) {
-  const soma = n1 + n2
+  const soma = n1 + n2;
   if (soma % 2 == 0) {
-    vitoriasPrimeiro += 1
-  } else{
-    vitoriasSegundo +=1
+    vitoriasPrimeiro += 1;
+  } else {
+    vitoriasSegundo += 1;
   }
-  console.log(soma)
+  console.log(soma);
 }
 
 for (let i = 0; i < primeiro.length; i++) {
-  resultados(primeiro[i], segundo[i])
+  resultados(primeiro[i], segundo[i]);
 }
 
-function venceu(vitoriasPrimeiro, vitoriasSegundo) {
+function venceu() {
   if (vitoriasPrimeiro > vitoriasSegundo) {
-    return vencedor = "PRIMEIRO"
+    return "PRIMEIRO";
   } else if (vitoriasSegundo > vitoriasPrimeiro) {
-    return vencedor = "SEGUNDO"
+    return "SEGUNDO";
   } else {
-    return vencedor = "EMPATE"
+    return "EMPATE";
   }
 }
 
-console.log(`DEU ${venceu(vitoriasPrimeiro, vitoriasSegundo)} \nPRIMEIRO ${vitoriasPrimeiro} X SEGUNDO ${vitoriasSegundo}.`)
+console.log(
+  `DEU ${venceu()} \nPRIMEIRO ${vitoriasPrimeiro} X SEGUNDO ${vitoriasSegundo}.`
+);
