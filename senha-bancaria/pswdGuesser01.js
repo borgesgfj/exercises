3fe57d5adcbae406a46b8105ac889c6db3d2fca5
Gilberto Borges <borgesgfj@gmail.com>
2021-07-16 11:14:12 -0300

    Vesões para entrada com 2 associações

--- senha-bancaria/pswdGuesser01.js
+++ senha-bancaria/pswdGuesser01.js
@@ -1,31 +1,31 @@
-const letters = ['A', 'B', 'C', 'D', 'E']
-const test1 = ['1739085624BCEAEB', '9075846231ECCBDA']
-const association1 = {}
-const association2 = {}
-let c = 0
+const letters = ["A", "B", "C", "D", "E"];
+const test1 = ["1739085624BCEAEB", "9075846231ECCBDA"];
+const association1 = {};
+const association2 = {};
+let c = 0;
 for (const item of letters) {
-  const correspondentNumbers_association1 = new Set()
-  const correspondentNumbers_association2 = new Set()
-  correspondentNumbers_association1.add(test1[0][c])
-  correspondentNumbers_association1.add(test1[0][c+1])
-  correspondentNumbers_association2.add(test1[1][c])
-  correspondentNumbers_association2.add(test1[1][c+1])
-  association1[item] = correspondentNumbers_association1
-  association2[item] = correspondentNumbers_association2
-  c += 2
+  const correspondentNumbers_association1 = new Set();
+  const correspondentNumbers_association2 = new Set();
+  correspondentNumbers_association1.add(test1[0][c]);
+  correspondentNumbers_association1.add(test1[0][c + 1]);
+  correspondentNumbers_association2.add(test1[1][c]);
+  correspondentNumbers_association2.add(test1[1][c + 1]);
+  association1[item] = correspondentNumbers_association1;
+  association2[item] = correspondentNumbers_association2;
+  c += 2;
 }
 
 function intersection(set1, set2) {
   for (const num of set1) {
     if (set2.has(num)) {
-      return num
+      return num;
     }
   }
 }
 
-let password = ''
+let password = "";
 for (let i = 10; i < 16; i++) {
-  password += (intersection(association1[test1[0][i]], association2[test1[1][i]]) + ' ')
+  password +=
+    intersection(association1[test1[0][i]], association2[test1[1][i]]) + " ";
 }
-console.log(`Teste 1\nA senha é: ${password}`)
-
+console.log(`Teste 1\nA senha é: ${password}`);
--- senha-bancaria/pswdGuesser02.js
+++ senha-bancaria/pswdGuesser02.js
@@ -1,34 +1,35 @@
-const letters = ['A', 'B', 'C', 'D', 'E']
-const test = ['1739085624BCEAEB', '9075846231ECCBDA']
+const letters = ["A", "B", "C", "D", "E"];
+const test = ["1739085624BCEAEB", "9075846231ECCBDA"];
 
 function creatSets(string, index) {
-  const setsOfAssociation = new Set()
-  setsOfAssociation.add(string[index])
-  setsOfAssociation.add(string[index + 1])
-  return setsOfAssociation
+  const setsOfAssociation = new Set();
+  setsOfAssociation.add(string[index]);
+  setsOfAssociation.add(string[index + 1]);
+  return setsOfAssociation;
 }
 
 function intersection(set1, set2) {
   for (const num of set1) {
     if (set2.has(num)) {
-      return num
+      return num;
     }
   }
 }
 
 for (let i = 0; i < test.length; i++) {
-  let charIndex = 0
-  eval("association_"+ i + "= {}")
+  let charIndex = 0;
+  eval("association_" + i + "= {}");
   for (const item of letters) {
-    eval("association_"+ i + "." + item + '=' + 'creatSets(test[i], charIndex)')
-    charIndex += 2
+    eval(
+      "association_" + i + "." + item + "=" + "creatSets(test[i], charIndex)"
+    );
+    charIndex += 2;
   }
 }
 
-let password = ''
+let password = "";
 for (let i = 10; i < 16; i++) {
-  password += (intersection(association_0[test[0][i]], association_1[test[1][i]]) + ' ')
+  password +=
+    intersection(association_0[test[0][i]], association_1[test[1][i]]) + " ";
 }
-console.log(`Teste 1\nA senha é: ${password}`)
-
-
+console.log(`Teste 1\nA senha é: ${password}`);
