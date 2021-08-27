35a68ba1e664d5c790430dcc46c56a0702849c75
borgesgfj <borgesgfj@gmail.com>
2021-08-27 13:45:59 -0300

    modified based on first PR turn discussion

--- quem-sera-reprovado/reprovado-v01.js
+++ quem-sera-reprovado/reprovado-v01.js
@@ -1,17 +1,15 @@
 const arrayEntry = [['cardonha', 9], ['infelizreprovado',3], ['marcel',9], ['infelizaprovado',3]]
 const secondCaseArrayEntry = [['zenon', 5], ['claudete',7], ['zezim', 3], ['anon',5], ['gilberto',10], ['belizaura', 3]]
-const classification = arrayEntry.sort(sort2dArray)
-const classificationSecondCase = secondCaseArrayEntry.sort(sort2dArray)
-function sort2dArray (a, b) {
-  if (a[1] == b[1]){
-    if (a[0] < b[0]) {
-      return -1
-    } else {
-      return 1
-    }
-  }else {
-    return b[1] - a[1]
+
+function sort2dArray (a, b) { /* a e b são duas entradas consecutivas do arrayEntry. Aqui são arrays do tipo: ['nome', nota].*/
+/* a[1] e b [1] fazem referêcnia aos elementos de índice 1 (as notas) nos arrays a e b se iguais: */
+/* se notas iguais analisa os nomes em ordem alfabética para ordenar */
+if (a[1] == b[1]){
+    return a[0] < b[0] ? -1 : 1
   }
+  return b[1] - a[1]
 }
-console.log(`Instancia 1\nA pessoa reprovada será: ${classification[classification.length - 1][0]}`)
-console.log(`Instancia 2\nA pessoa reprovada será: ${classificationSecondCase[classificationSecondCase.length - 1][0]}`)
+arrayEntry.sort(sort2dArray)
+secondCaseArrayEntry.sort(sort2dArray)
+console.log(`Instancia 1\nA pessoa reprovada será: ${arrayEntry[arrayEntry.length - 1][0]}`)
+console.log(`Instancia 2\nA pessoa reprovada será: ${secondCaseArrayEntry[secondCaseArrayEntry.length - 1][0]}`)
