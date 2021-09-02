ef0ba87f844e50025dc5525ef189c5cd95136b7d
borgesgfj <borgesgfj@gmail.com>
2021-09-02 10:29:43 -0300

    add problem statement and change the name of variable sequence

--- magic-bracelets/forbidden-bracelet-app01.js
+++ magic-bracelets/forbidden-bracelet-app01.js
@@ -8,32 +8,31 @@ const testCases = [
     braceletSequence: "av",
   },
   {
-    forbiddenSequence: 'haskell',
-    braceletSequence: 'lleksah'
+    forbiddenSequence: "haskell",
+    braceletSequence: "lleksah",
   },
   {
-    forbiddenSequence: 'isitfriday',
-    braceletSequence: 'ohnoitisnt'
+    forbiddenSequence: "isitfriday",
+    braceletSequence: "ohnoitisnt",
   },
   {
-    forbiddenSequence: 'itsfriday',
-    braceletSequence: 'fridaygottagetdownonfriday'
+    forbiddenSequence: "itsfriday",
+    braceletSequence: "fridaygottagetdownonfriday",
   },
   {
-    forbiddenSequence: 'yyt',
-    braceletSequence: 'ttyttyttyytty'
-  }
+    forbiddenSequence: "yyt",
+    braceletSequence: "ttyttyttyytty",
+  },
 ];
 
 for (let i = 0; i < testCases.length; i++) {
-  const sequence = testCases[i].braceletSequence.repeat(2);
-  const inverseSequence = sequence.split("").reverse("").join("");
+  const circularSequence = testCases[i].braceletSequence.repeat(2);
+  const inverseSequence = circularSequence.split("").reverse("").join("");
   const isForbidden =
-    sequence.includes(testCases[i].forbiddenSequence) ||
+    circularSequence.includes(testCases[i].forbiddenSequence) ||
     inverseSequence.includes(testCases[i].forbiddenSequence)
       ? "S"
       : "N";
   console.log(testCases[i].braceletSequence);
   console.log(isForbidden);
 }
-
