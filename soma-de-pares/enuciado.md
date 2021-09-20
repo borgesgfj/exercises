7974efac927b4f1397b31149277fd5aef2999f9f
Gilberto Borges <57193296+borgesgfj@users.noreply.github.com>
2021-09-20 19:12:49 -0300

    Problem sum of pairs (#4)
    * solved by interating over two lists

---
+++ soma-de-pares/enuciado.md
@@ -0,0 +1,4 @@
+## Soma de pares
+
+
+Dado duas listas com números inteiros e um inteiro X, retornar os pares de números das listas cuja a soma é igual a X.
---
+++ soma-de-pares/somaDePares.js
@@ -0,0 +1,15 @@
+const testCase = [[1, 3, 4, 5, 6, 2, 11], [1,3, 8, 5, 9]]
+const sumResult = 10
+function selectNumberPairs(arrayNumbers1, arrayNumbers2) {
+  let validPairs = []
+  for (let i = 0; i < arrayNumbers1.length; i++) {
+    for (let j = 0; j < arrayNumbers2.length; j++){
+      if (arrayNumbers1[i] + arrayNumbers2[j] == sumResult) {
+        validPairs.push([arrayNumbers1[i], arrayNumbers2[j]])
+      }
+    }
+  }
+  return validPairs
+}
+
+console.log(selectNumberPairs(testCase[0], testCase[1]))
